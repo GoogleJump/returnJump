4b8042f8bb99d500e5ae4511c2b3eb1a7bc14742
Tasti Zakarie <tasti.zakarie@gmail.com>
2014-03-07 18:20:13 -0500

    test .gitignore

--- spoilFoil/gen/com/returnjump/spoilfoil/R.java
+++ spoilFoil/gen/com/returnjump/spoilfoil/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -45,13 +45,17 @@ public final class R {
         public static final int tasti=0x7f070004;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_arturo=0x7f050004;
-        public static final int title_activity_jeffrey=0x7f050006;
-        public static final int title_activity_kelsey=0x7f050005;
-        public static final int title_activity_tasti=0x7f050003;
+        public static final int action_settings=0x7f040005;
+        public static final int app_name=0x7f040004;
+        public static final int hello_world=0x7f040006;
+        public static final int parse_app_id=0x7f040002;
+        public static final int parse_app_id_fake=0x7f040000;
+        public static final int parse_client_key=0x7f040003;
+        public static final int parse_client_key_fake=0x7f040001;
+        public static final int title_activity_arturo=0x7f040008;
+        public static final int title_activity_jeffrey=0x7f04000a;
+        public static final int title_activity_kelsey=0x7f040009;
+        public static final int title_activity_tasti=0x7f040007;
     }
     public static final class style {
         /** 
