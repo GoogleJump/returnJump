29dbf76cf24518d5a17766ca65a454951e3cbaae
Tasti Zakarie <tasti.zakarie@gmail.com>
2014-04-06 22:54:35 -0400

    cleanup

--- spoilFoil/gen/com/returnjump/spoilfoil/R.java
+++ spoilFoil/gen/com/returnjump/spoilfoil/R.java
@@ -64,16 +64,16 @@ public final class R {
         public static final int captured_image=0x7f04000c;
         public static final int edit_text_DaysGood=0x7f04000f;
         public static final int edit_text_new_foodItem=0x7f04000e;
-        public static final int hello_world=0x7f040006;
+        public static final int hello_world=0x7f04000a;
         public static final int parse_app_id=0x7f040002;
         public static final int parse_app_id_fake=0x7f040000;
         public static final int parse_client_key=0x7f040003;
         public static final int parse_client_key_fake=0x7f040001;
-        public static final int processing_image=0x7f040007;
-        public static final int title_activity_arturo=0x7f040009;
-        public static final int title_activity_jeffrey=0x7f04000b;
-        public static final int title_activity_kelsey=0x7f04000a;
-        public static final int title_activity_tasti=0x7f040008;
+        public static final int processing_image=0x7f04000b;
+        public static final int title_activity_arturo=0x7f040007;
+        public static final int title_activity_jeffrey=0x7f040009;
+        public static final int title_activity_kelsey=0x7f040008;
+        public static final int title_activity_tasti=0x7f040006;
         public static final int your_food_items=0x7f04000d;
     }
     public static final class style {
