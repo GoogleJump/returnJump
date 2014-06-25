5a72cbf08c352f681f96e2b036cc32ac9753af56
Tasti Zakarie <tasti.zakarie@gmail.com>
2014-06-24 22:26:20 -0400

    I swear this fixes it

--- spoilFoil/src/com/returnjump/spoilfoil/FridgeDbHelper.java
+++ spoilFoil/src/com/returnjump/spoilfoil/FridgeDbHelper.java
@@ -244,7 +244,7 @@ public class FridgeDbHelper extends SQLiteOpenHelper {
 
         Example
             To get all items with expiry date before Jan. 1, 2014 and haven't been dismissed:
-                dbHelper.getAll(new String[]{DatabaseContract.FridgeTable.COLUMN_NAME_EXPIRY_DATE, DatabaseContract.FridgeTable.COLUMN_NAME_DISMISSED}, new String[]{DatabaseContract.AND}, new String[]{"<", "="}, new String[]{"2014-01-01", DatabaseContract.BOOL_FALSE_STR}, true);
+                dbHelper.getAll(new String[]{DatabaseContract.FridgeTable.COLUMN_NAME_EXPIRY_DATE, DatabaseContract.FridgeTable.COLUMN_NAME_DISMISSED}, new String[]{"<", "="}, new String[]{"2014-01-01", DatabaseContract.BOOL_FALSE_STR}, new String[]{DatabaseContract.AND}, true);
 
      */
     public List<FridgeItem> getAll(String[] column, String[] operator, String[] whereValue, String[] conjunction, boolean isMinimal) {
