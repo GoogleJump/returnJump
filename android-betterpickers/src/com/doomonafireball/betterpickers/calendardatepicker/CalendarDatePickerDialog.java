6a031f52d0f1831662951e4a3332740449fcb417
khrubes <kelseyhrubes@gmail.com>
2014-07-02 20:33:41 -0500

    Adds undobar as submodule, fixes bug with calendar picker

--- android-betterpickers/src/com/doomonafireball/betterpickers/calendardatepicker/CalendarDatePickerDialog.java
+++ android-betterpickers/src/com/doomonafireball/betterpickers/calendardatepicker/CalendarDatePickerDialog.java
@@ -162,21 +162,7 @@ public class CalendarDatePickerDialog extends DialogFragment implements
     public boolean selectedDateBeforeToday() {
         Calendar c = Calendar.getInstance();
         int  compare = mCalendar.compareTo(c);
-
-        String days = mCalendar.get(Calendar.DAY_OF_MONTH) +"     and today" + c.get(Calendar.DAY_OF_MONTH);
-
-
-        if ((mCalendar.get(Calendar.MONTH) <= c.get(Calendar.MONTH)) && c.get(Calendar.MONTH) != 11) {
-            if (mCalendar.get(Calendar.DAY_OF_MONTH) < c.get(Calendar.DAY_OF_MONTH)) {
-                return true;
-            }else if ((mCalendar.get(Calendar.MONTH) < c.get(Calendar.MONTH))){
-                return true;
-            }
-
-        }
-        return false;
-
-
+        return compare < 0;
     }
 
     @Override
