80596a455c752677f38c86723ab3cadeb0c1462b
1035AMC <arturo.1035@gmail.com>
2014-08-06 02:01:05 -0300

    reverted the changes made

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -4,21 +4,21 @@ Parse.Cloud.job("emailTask", function(request, response) {
     query.find({
         success: function(results) {
             for (var i = 0; i < results.length; i++) {
-                console.log(i)
                 var all_items = null;
                 //getting the information required to call the email sender function
                 users_email = results[i].get("email");
                 if ((results[i].get("notifyEmail") === true) && !(users_email === undefined || users_email === "")) {
-                    console.log("in if statement");
                     console.log(users_email);
                     //acquiring the email adress
                     //acquiring the items that are about to expire
                     user_install_id = results[i].get("installationObjectId");
+                    prefTime = results[i].get("notifyTime");
                     Parse.Cloud.run("execute", {
                         ObjectId: user_install_id,
-                        email: users_email
-                    }); 
-                    if (i== results.length -1){
+                        email: users_email,
+                        time: prefTime
+                    });
+                    if (i == results.length - 1) {
                         response.success("Done");
                     }
 
@@ -37,30 +37,46 @@ Parse.Cloud.job("emailTask", function(request, response) {
 });
 
 Parse.Cloud.define('execute', function(request, response) {
+    var moment = require("moment");
+    ObjectId = request.params.ObjectId;
     Parse.Cloud.run("getExpired", {
-        user_param: request.params.ObjectId
+        user_param: ObjectId
     }).then(function(expiredItems) {
-        console.log("in the promise");
-        if (!(expiredItems === "")) {
-            Parse.Cloud.run("sendEmail", {
-                email: request.params.email,
-                expiredFridgeItems: expiredItems
-            }, {
-                success: function() {
-                    response.success("Emails sent");
-                },
-                error: function(error) {
-                    response.error("Error" + error.code + " " + error.message);
-                }
-            });
-        }
-          else{
-            response.success("no items to notify of");
+            if (!(expiredItems === "" )) {
+                Parse.Cloud.run("sendEmail", {
+                    email: request.params.email,
+                    expiredFridgeItems: expiredItems
+                }, {
+                    success: function() {
+                        response.success("Emails sent");
+                    },
+                    error: function(error) {
+                        response.error("Error" + error.code + " " + error.message);
+                    }
+                });
+            } else {
+                response.success("no items to notify of");
             }
-    }), function(error){
-        response.error("Error" + error.code + " " + error.message);
-    }
+    });
 });
+
+//getTimezone of user 
+
+Parse.Cloud.define("getTimezone", function(request, response) {
+    var Installation = Parse.Object.extend("Installation");
+    var installation_query = new Parse.Query(Installation);
+    installation_query.get(request.params.ObjectId, {
+        success: function(result){
+            response.success(result.get("notifyTime"));
+        },
+        error: function(o, error){
+            response.error(error);
+        }
+    });
+    console.log(request.params.ObjectId);
+});
+
+//Get all items that are expiring for a specific user
 
 Parse.Cloud.define("getExpired", function(request, response) {
     //date format
@@ -81,7 +97,7 @@ Parse.Cloud.define("getExpired", function(request, response) {
     var query = new Parse.Query(Fridge);
     query.equalTo("installationObjectId", request.params.user_param);
     query.equalTo("expiryDate", date_string);
-    query.equalTo("notifiedEmail", false);  
+    query.equalTo("notifiedEmail", false);
     query.find({
         success: function(results) {
             var namesOfExpiringItems = [];
@@ -90,10 +106,12 @@ Parse.Cloud.define("getExpired", function(request, response) {
                 // get the names of the items that are expiring
                 namesOfExpiringItems[i] = results[i].get("name");
                 // update the item to notified.
-                // results[i].save(null, {success: function(){
-                //     results[i].set("notifiedEmail", true);
-                //     results[i].save();
-                // }})
+                results[i].save(null, {
+                    success: function() {
+                        results[i].set("notifiedEmail", true);
+                        results[i].save();
+                    }
+                })
             }
             response.success(namesOfExpiringItems);
         },
@@ -144,32 +162,32 @@ Parse.Cloud.define('sendEmail', function(request, response) {
     var query = new Parse.Query('Configuration');
     query.equalTo('key', 'api_key_website');
     query.first({
-      success: function(config) {
-        Parse.Cloud.httpRequest({
-            method: 'POST',
-            url: 'http://frij-app.appspot.com/api/email',
-            headers: {
-                'Content-Type': 'application/json',
-                'API_KEY': config.get('value')
-            },
-            body: {
-                email: request.params.email,
-                expiredFridgeItems: request.params.expiredFridgeItems
-            },
-            success: function(httpResponse) {
-                console.log(httpResponse.text);
-                response.success(httpResponse.text);
-            },
-            error: function(httpResponse) {
-                console.error('Request failed with response code ' + httpResponse.status);
-                response.error('Request failed with response code ' + httpResponse.status);
-            }
-        });
-      },
-      error: function(error) {
-        console.error('Error: ' + error.code + ' ' + error.message);
-        response.error('Error: ' + error.code + ' ' + error.message);
-      }
+        success: function(config) {
+            Parse.Cloud.httpRequest({
+                method: 'POST',
+                url: 'http://frij-app.appspot.com/api/email',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'API_KEY': config.get('value')
+                },
+                body: {
+                    email: request.params.email,
+                    expiredFridgeItems: request.params.expiredFridgeItems
+                },
+                success: function(httpResponse) {
+                    console.log(httpResponse.text);
+                    response.success(httpResponse.text);
+                },
+                error: function(httpResponse) {
+                    console.error('Request failed with response code ' + httpResponse.status);
+                    response.error('Request failed with response code ' + httpResponse.status);
+                }
+            });
+        },
+        error: function(error) {
+            console.error('Error: ' + error.code + ' ' + error.message);
+            response.error('Error: ' + error.code + ' ' + error.message);
+        }
     });
 
 });
