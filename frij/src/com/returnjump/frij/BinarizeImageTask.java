f5e21ec771ec75b532d9a701cfa394831caa4a30
Jeffrey Lai <jeffrey.lai@yale.edu>
2014-08-05 18:07:48 -0700

    Added dustoff for binarize Image

--- frij/src/com/returnjump/frij/BinarizeImageTask.java
+++ frij/src/com/returnjump/frij/BinarizeImageTask.java
@@ -6,9 +6,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 import android.widget.Toast;
+import java.awt.Point;
+import java.util.LinkedList;
 
 import com.googlecode.leptonica.android.Binarize;
 import com.googlecode.leptonica.android.Pix;
@@ -43,7 +46,8 @@ public class BinarizeImageTask extends AsyncTask<String, Void, Bitmap> {
         pix = Binarize.otsuAdaptiveThreshold(pix, 32, 32, 2, 2, 0.9F);
         //pix = Binarize.otsuAdaptiveThreshold(pix);
         bitmap = WriteFile.writeBitmap(pix);
-
+        //remove dust here
+        dustOff(bitmap);
         return bitmap;
     }
 
@@ -65,5 +69,48 @@ public class BinarizeImageTask extends AsyncTask<String, Void, Bitmap> {
                 .show();
 
         new OcrImageTask(true, context, activity).execute(bitmap);
+    }
+
+    protected int numBlackNeighbors(Bitmap b, int x, int y) {
+        int count = 0;
+        if(x+1 < b.getWidth())
+            count += b.getPixel(x+1, y) == Color.BLACK ? 1 : 0;
+        if(y+1 < b.getHeight())
+            count += b.getPixel(x, y+1) == Color.BLACK ? 1 : 0;
+        if(x-1 > -1)
+            count += b.getPixel(x-1, y) == Color.BLACK ? 1 : 0;
+        if(y-1 > -1)
+            count += b.getPixel(x, y-1) == Color.BLACK ? 1 : 0;
+        return count;
+    }
+    protected Point getBlackNeighbor(Bitmap b, int x, int y) {
+        if(x+1 < b.getWidth() && b.getPixel(x+1, y) == Color.BLACK)
+            return new Point(x+1, y);
+        if(y+1 < b.getWidth() && b.getPixel(x, y+1) == Color.BLACK)
+            return new Point(x, y+1);
+        if(x-1 > -1 && b.getPixel(x-1, y) == Color.BLACK)
+            return new Point(x-1, y);
+        if(y-1 < -1 && b.getPixel(x, y-1) == Color.BLACK)
+            return new Point(x, y-1);
+        return null;
+    }
+
+    protected void dustOff(Bitmap b) {
+        LinkedList<Point> blackPixels = new LinkedList<Point>();
+        for(int x = 0; x < b.getWidth(); x++) {
+            for(int y = 0; y < b.getHeight(); y++) {
+                if(b.getPixel(x, y) == Color.Black)
+                    blackPixels.add(new Point(x, y));
+            }
+        }
+        while(blackPixels.size() != 0) {
+            Point p = blackPixels.removeFirst();
+            if(numBlackNeighbors(b, p.x, p.y) < 2) {
+                b.setPixel(p.x, p.y, Color.WHITE);
+                Point temp = getBlackNeighbor(b, p.x, p.y);
+                if(temp != null && !blackPixels.contains(temp))
+                    blackPixels.addLast(temp);
+            }
+        }
     }
 }
